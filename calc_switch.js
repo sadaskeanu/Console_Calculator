bdd80a8b9e44e8c1e4c373cfbed7b5ed10fb81ac
Daria <79363439+sadaskeanu@users.noreply.github.com>
2022-01-23 16:14:36 +0200

    Add files via upload
    Код исправлен согласно ТЗ

---
+++ calc_switch.js
@@ -0,0 +1,26 @@
+
+
+function Calc (operation, a, b) {
+
+    let isValid = typeof a != 'number' || typeof b != 'number';
+
+    if (isValid) {
+        return 'Error';
+    } 
+
+    switch (operation) {
+        case 'sum' : return a + b;
+        case 'sub' : return a - b;
+        case 'div' : return a / b;
+        case 'mult' : return a * b;
+        case 'rem' : return a % b;
+        case 'exp' : return a ** b;
+        default : return 'unknown operation';
+
+}
+}
+
+let result = Calc('exp',2,8);
+console.log(result);
+
+
